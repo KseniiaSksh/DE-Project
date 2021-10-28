292a06747a8590550d46bf5e88f61f5103f1f162
Taras Smaliukh <taras.smalych@gmail.com>
2021-10-28 19:38:38 +0300

    transferring  data from rds to s3 in csv - done

--- config.py
+++ config.py
@@ -1,10 +1,17 @@
-
 import mysql.connector
 
-# Connection to db
-def getConnection():
+import boto
+import boto.s3.connection
+
+# Connection to db on rds
+def getConnectionRDS():
     cnx = mysql.connector.connect(user='taras',
                               password='taras_smaliukh',
                               host='dbnorthwind.cjgt1chgkycw.eu-central-1.rds.amazonaws.com',
                               database='northwind')
     return cnx
+
+# Credentials to connect S3
+bucket_name = 'delabnortwind'
+access_key = 'AKIAZNBEQCNNE3EWRMMG'
+secret_key = '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'
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import csv
+import io
+import pandas as pd
+import boto
+import os
+import boto3
+import s3fs as s3fs
+from boto3 import Session
+
 import config
 
 import mysql.connector
@@ -15,7 +24,7 @@ import re
 def connection_to_db():
     connection_db = None
     try:
-        connection = config.getConnection()
+        connection = config.getConnectionRDS()
         print("Connect successful!")
 
     except Error as e:
@@ -23,9 +32,13 @@ def connection_to_db():
 
     return connection
 
-    # connection.close()
-    # print("Close")
 
+def upload_file(file_name, bucket, object_name=None, args=None):
+    if object_name is None:
+        object_name = file_name
+
+    client_S3.upload_file(file_name, bucket, object_name, ExtraArgs=args)
+    print(f"'{file_name}' has been uploaded to '{config.bucket_name}'")
 
 conn = connection_to_db()
 cursor = conn.cursor()
@@ -40,15 +53,54 @@ for table in query_result:
         table_list.append(i)
 print(table_list)
 
-get_data_from_tables = f"SELECT * FROM {table_list[0]}"
-print(f'Data from table {table_list[0]}')
-cursor.execute(get_data_from_tables)
-query_result2 = cursor.fetchall()
-for row in query_result2:
-    print("CategotyID = ", row[0])
-    print("CategotyName = ", row[1])
-    print("Description = ", row[2])
-print("End of Data")
+client_S3 = boto3.client(
+    's3',
+    aws_access_key_id = config.access_key,
+    aws_secret_access_key = config.secret_key
+)
+
+session = Session(aws_access_key_id=config.access_key,
+                  aws_secret_access_key=config.secret_key)
+s3 = session.resource('s3')
+your_bucket = s3.Bucket(config.bucket_name)
+
+## Show files in the bucket
+# for s3_file in your_bucket.objects.all():
+#     print(s3_file.key)
+
+
+try:
+    cursor.execute(f"SELECT * FROM {table_list[1]};")
+    rows = cursor.fetchall()
+finally:
+    conn.close()
+
+# Continue only if there are rows returned.
+
+csv_file = f'{table_list[1]}_actualdate.csv'
+
+s3 = s3fs.S3FileSystem(anon=False)
+
+
+if rows:
+    # New empty list called 'result'. This will be written to a file.
+    result = list()
+
+    # The row name is the first entry for each entity in the description tuple.
+    column_names = list()
+    for i in cursor.description:
+        column_names.append(i[0])
+
+    result.append(column_names)
+    for row in rows:
+        result.append(row)
+
+    # Write result to file.
+    with open(csv_file, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for row in result:
+            csvwriter.writerow(row)
+        upload_file(csv_file, config.bucket_name)
 
 
 cursor.close()
