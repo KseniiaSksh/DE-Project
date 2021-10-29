701dbe7d6630cc2b63f5885809c1b17302bff6da
Oksana <oksana5ostrovska@gmail.com>
2021-10-29 15:58:21 +0300

    Added type casting, without date dimension

--- script.sql
+++ script.sql
@@ -1,16 +1,21 @@
-
 --drop unnecessary tables
 DROP TABLE IF EXISTS Territories;
+
 DROP TABLE IF EXISTS Region;
+
 DROP TABLE IF EXISTS Contacts;
 
-ALTER TABLE Shippers 
-ADD PRIMARY KEY (ShipperID);
+ALTER TABLE
+    Shippers
+ADD
+    PRIMARY KEY (ShipperID);
 
 --CUSTOMERS
-CREATE OR REPLACE TABLE Customers AS
-SELECT CustomerID AS CustomerSK,
-    CustomerID,
+CREATE
+OR REPLACE TABLE Customers AS
+SELECT
+    CustomerID AS CustomerSK,
+    CustomerID
     CompanyName,
     ContactName,
     ContactTitle,
@@ -22,15 +27,20 @@ SELECT CustomerID AS CustomerSK,
     1 AS CurrentFlag,
     CURRENT_TIMESTAMP(2) AS RecordStartDate,
     CURRENT_TIMESTAMP(2) AS RecordEndDate
-FROM Customers;
+FROM
+    Customers;
 
-ALTER TABLE Customers 
-ADD PRIMARY KEY (CustomerSK);
+ALTER TABLE
+    Customers
+ADD
+    PRIMARY KEY (CustomerSK);
 
 --EMPLOYEES
-CREATE OR REPLACE TABLE Employees AS
-SELECT EmployeeID AS EmployeeSK,
-    EmployeeID,
+CREATE
+OR REPLACE TABLE Employees AS
+SELECT
+    TRY_TO_NUMBER(EmployeeID) AS EmployeeSK,
+    TRY_TO_NUMBER(EmployeeID) AS EmployeeID,
     LastName,
     FirstName,
     Title,
@@ -47,15 +57,19 @@ SELECT EmployeeID AS EmployeeSK,
     1 AS CurrentFlag,
     CURRENT_TIMESTAMP(2) AS RecordStartDate,
     CURRENT_TIMESTAMP(2) AS RecordEndDate
-FROM Employees;
+FROM
+    Employees;
 
-ALTER TABLE Employees 
-ADD PRIMARY KEY (EmployeeSK);
-
+ALTER TABLE
+    Employees
+ADD
+    PRIMARY KEY (EmployeeSK);
 
 --SUPPLIERS
-CREATE OR REPLACE TABLE Suppliers AS
-SELECT SupplierID AS SupplierSK,
+CREATE
+OR REPLACE TABLE Suppliers AS
+SELECT
+    SupplierID AS SupplierSK,
     SupplierID,
     CompanyName,
     ContactName,
@@ -68,68 +82,87 @@ SELECT SupplierID AS SupplierSK,
     1 AS CurrentFlag,
     CURRENT_TIMESTAMP(2) AS RecordStartDate,
     CURRENT_TIMESTAMP(2) AS RecordEndDate
-FROM Suppliers;
+FROM
+    Suppliers;
 
-ALTER TABLE Suppliers 
-ADD PRIMARY KEY (SupplierSK);
-
+ALTER TABLE
+    Suppliers
+ADD
+    PRIMARY KEY (SupplierSK);
 
 --ORDER DETAILS
 CREATE TABLE OrderDetails AS
-SELECT Orders.OrderID,
-    ProductID AS ProductSK,
+SELECT
+    TRY_TO_NUMBER(Orders.OrderID) AS OrderID,
+    TRY_TO_NUMBER(ProductID) AS ProductSK,
     CustomerID AS CustomerSK,
-    EmployeeID AS EmployeeSK,
-    ShipVia AS ShipperID,
-    TRY_TO_TIMESTAMP(OrderDate) AS OrderDateKey,
-    TRY_TO_TIMESTAMP(RequiredDate) AS RequiredDateKey,
-    TRY_TO_TIMESTAMP(ShippedDate) AS ShippedDateKey,
-    UnitPrice AS ProductUnitPrice,
-    Quantity AS ProductQuantity,
-    Discount AS ProductDiscount,
-    Freight,
+    TRY_TO_NUMBER(EmployeeID) AS EmployeeSK,
+    TRY_TO_NUMBER(ShipVia) AS ShipperID,
+    TRY_TO_DATE(OrderDate) AS OrderDateKey,
+    TRY_TO_DATE(RequiredDate) AS RequiredDateKey,
+    TRY_TO_DATE(ShippedDate) AS ShippedDateKey,
+    TRY_TO_NUMBER(UnitPrice) AS ProductUnitPrice,
+    TRY_TO_NUMBER(Quantity) AS ProductQuantity,
+    TRY_TO_NUMBER(Discount) AS ProductDiscount,
+    TRY_TO_NUMBER(Freight) AS Freight,
     ShipName,
     ShipAddress,
     ShipCity,
     ShipRegion,
     ShipPostalCode,
     ShipCountry
-FROM Orders
-JOIN [Order Details] AS od
-ON Orders.OrderID = od.OrderId;
+FROM
+    Orders
+    JOIN [Order Details] AS od ON Orders.OrderID = od.OrderId;
 
-ALTER TABLE OrderDetails
-ADD FOREIGN KEY (ProductSK) REFERENCES Products(ProductSK);
+ALTER TABLE
+    OrderDetails
+ADD
+    FOREIGN KEY (ProductSK) REFERENCES Products(ProductSK);
 
-ALTER TABLE OrderDetails
-ADD FOREIGN KEY (CustomerSK) REFERENCES Customers(CustmoerSK);
+ALTER TABLE
+    OrderDetails
+ADD
+    FOREIGN KEY (CustomerSK) REFERENCES Customers(CustmoerSK);
 
-ALTER TABLE OrderDetails
-ADD FOREIGN KEY (EmployeeSK) REFERENCES Employees(EmployeeSK);
+ALTER TABLE
+    OrderDetails
+ADD
+    FOREIGN KEY (EmployeeSK) REFERENCES Employees(EmployeeSK);
 
-ALTER TABLE OrderDetails
-ADD FOREIGN KEY (ShipperID) REFERENCES Shippers(ShipperID);
+ALTER TABLE
+    OrderDetails
+ADD
+    FOREIGN KEY (ShipperID) REFERENCES Shippers(ShipperID);
 
-ALTER TABLE OrderDetails
-ADD PRIMARY KEY (OrderID, ProductSK)
+ALTER TABLE
+    OrderDetails
+ADD
+    PRIMARY KEY (OrderID, ProductSK);
 
 --PRODUCTS
-CREATE OR REPLACE TABLE Products AS
-SELECT ProductID AS ProductSK,
-    ProductID,
+CREATE
+OR REPLACE TABLE Products AS
+SELECT
+    TRY_TO_NUMBER(ProductID) AS ProductSK,
+    TRY_TO_NUMBER(ProductID) AS ProductID,
     ProductName,
-    QuantityPerUnit,
-    UnitPrice,
-    ReorderLevel,
+    TQuantityPerUnit),
+    TRY_TO_NUMBER(UnitPrice) AS UnitPrice,
+    RY_TO_NUMBER(ReorderLevel) AS ReorderLevel,
     Discontinued,
     CategoryName,
     Description AS CategoryDescription,
     1 AS CurrentFlag,
     CURRENT_TIMESTAMP(2) AS RecordStartDate,
     CURRENT_TIMESTAMP(2) AS RecordEndDate
-FROM Products
-JOIN Categories
-ON Products.CategoryID = Categories.CategoryID;
+FROM
+    Products
+    JOIN Categories ON Products.CategoryID = Categories.CategoryID;
 
-ALTER TABLE Products 
-ADD PRIMARY KEY (ProductSK);
+ALTER TABLE
+    Products
+ADD
+    PRIMARY KEY (ProductSK);
+
+CREATE TABLE DateDimension
