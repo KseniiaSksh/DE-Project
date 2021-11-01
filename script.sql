dc954bf08befcb243ae44b096d392dc034a923e9
Oksana <oksana5ostrovska@gmail.com>
2021-11-01 11:12:19 +0200

    for verification

--- script.sql
+++ script.sql
@@ -1,168 +1,106 @@
---drop unnecessary tables
-DROP TABLE IF EXISTS Territories;
-
-DROP TABLE IF EXISTS Region;
-
-DROP TABLE IF EXISTS Contacts;
-
-ALTER TABLE
-    Shippers
-ADD
-    PRIMARY KEY (ShipperID);
-
---CUSTOMERS
 CREATE
-OR REPLACE TABLE Customers AS
-SELECT
-    CustomerID AS CustomerSK,
-    CustomerID
-    CompanyName,
-    ContactName,
-    ContactTitle,
-    Address,
-    City,
-    Region,
-    PostalCode,
-    Country,
-    1 AS CurrentFlag,
-    CURRENT_TIMESTAMP(2) AS RecordStartDate,
-    CURRENT_TIMESTAMP(2) AS RecordEndDate
-FROM
-    Customers;
+OR REPLACE TABLE Shippers (
+    ShipperID int PRIMARY KEY,
+    CompanyName varchar(40) NOT NULL,
+    Phone varchar(24)
+);
 
-ALTER TABLE
-    Customers
-ADD
-    PRIMARY KEY (CustomerSK);
+CREATE
+OR REPLACE TABLE Customers (
+    CustomerSK int DEFAULT AUTOINCREMENT(1, 1) PRIMARY KEY,
+    CustomerID varchar(5) NOT NULL,
+    CompanyName varchar(40) NOT NULL,
+    ContactName varchar(30),
+    ContactTitle varchar(30),
+    Address varchar(60),
+    City varchar(15),
+    Region varchar(15),
+    PostalCode varchar(10),
+    Country varchar(15),
+    CurrentFlag boolean DEFAULT 1,
+    RecordStartDate date DEFAULT CURRENT_DATE(),
+    RecordStartDate date DEFAULT CURRENT_DATE()
+);
 
 --EMPLOYEES
 CREATE
-OR REPLACE TABLE Employees AS
-SELECT
-    TRY_TO_NUMBER(EmployeeID) AS EmployeeSK,
-    TRY_TO_NUMBER(EmployeeID) AS EmployeeID,
-    LastName,
-    FirstName,
-    Title,
-    TitleOfCourtesy,
-    TRY_TO_DATE(BirthDate),
-    TRY_TO_DATE(HireDate),
-    Address,
-    City,
-    Region,
-    PostalCode,
-    Country,
-    Notes,
-    ReportsTo,
-    1 AS CurrentFlag,
-    CURRENT_TIMESTAMP(2) AS RecordStartDate,
-    CURRENT_TIMESTAMP(2) AS RecordEndDate
-FROM
-    Employees;
+OR REPLACE TABLE Employees (
+    EmployeeSK int DEFAULT AUTOINCREMENT(1, 1) PRIMARY KEY,
+    EmployeeID int NOT NULL,
+    LastName varchar(20) NOT NULL,
+    FirstName varchar(10) NOT NULL,
+    Title varchar(30),
+    TitleOfCourtesy varchar(25),
+    BirthDate date,
+    HireDate date,
+    Address varchar(60),
+    City varchar(15),
+    Region varchar(15),
+    PostalCode varchar(10),
+    Country varchar(15),
+    Notes text,
+    ReportsTo int,
+    CurrentFlag boolean DEFAULT 1,
+    RecordStartDate date DEFAULT CURRENT_DATE(),
+    RecordStartDate date DEFAULT CURRENT_DATE()
+);
 
-ALTER TABLE
-    Employees
-ADD
-    PRIMARY KEY (EmployeeSK);
+PRIMARY KEY (EmployeeSK);
 
 --SUPPLIERS
 CREATE
-OR REPLACE TABLE Suppliers AS
-SELECT
-    SupplierID AS SupplierSK,
-    SupplierID,
-    CompanyName,
-    ContactName,
-    ContactTitle,
-    Address,
-    City,
-    Region,
-    PostalCode,
-    Country,
-    1 AS CurrentFlag,
-    CURRENT_TIMESTAMP(2) AS RecordStartDate,
-    CURRENT_TIMESTAMP(2) AS RecordEndDate
-FROM
-    Suppliers;
-
-ALTER TABLE
-    Suppliers
-ADD
-    PRIMARY KEY (SupplierSK);
+OR REPLACE TABLE Suppliers (
+    SupplierSK int DEFAULT AUTOINCREMENT(1, 1) PRIMARY KEY,
+    SupplierID int NOT NULL,
+    CompanyName varchar(40) NOT NULL,
+    ContactName varchar(30),
+    ContactTitle varchar(30),
+    Address varchar(60),
+    City varchar(15),
+    Region varchar(15),
+    PostalCode varchar(10),
+    Country varchar(15),
+    CurrentFlag boolean DEFAULT 1,
+    RecordStartDate date DEFAULT CURRENT_DATE(),
+    RecordStartDate date DEFAULT CURRENT_DATE()
+);
 
 --ORDER DETAILS
-CREATE TABLE OrderDetails AS
-SELECT
-    TRY_TO_NUMBER(Orders.OrderID) AS OrderID,
-    TRY_TO_NUMBER(ProductID) AS ProductSK,
-    CustomerID AS CustomerSK,
-    TRY_TO_NUMBER(EmployeeID) AS EmployeeSK,
-    TRY_TO_NUMBER(ShipVia) AS ShipperID,
-    TRY_TO_DATE(OrderDate) AS OrderDateKey,
-    TRY_TO_DATE(RequiredDate) AS RequiredDateKey,
-    TRY_TO_DATE(ShippedDate) AS ShippedDateKey,
-    TRY_TO_NUMBER(UnitPrice) AS ProductUnitPrice,
-    TRY_TO_NUMBER(Quantity) AS ProductQuantity,
-    TRY_TO_NUMBER(Discount) AS ProductDiscount,
-    TRY_TO_NUMBER(Freight) AS Freight,
-    ShipName,
-    ShipAddress,
-    ShipCity,
-    ShipRegion,
-    ShipPostalCode,
-    ShipCountry
-FROM
-    Orders
-    JOIN [Order Details] AS od ON Orders.OrderID = od.OrderId;
-
-ALTER TABLE
-    OrderDetails
-ADD
-    FOREIGN KEY (ProductSK) REFERENCES Products(ProductSK);
-
-ALTER TABLE
-    OrderDetails
-ADD
-    FOREIGN KEY (CustomerSK) REFERENCES Customers(CustmoerSK);
-
-ALTER TABLE
-    OrderDetails
-ADD
-    FOREIGN KEY (EmployeeSK) REFERENCES Employees(EmployeeSK);
-
-ALTER TABLE
-    OrderDetails
-ADD
-    FOREIGN KEY (ShipperID) REFERENCES Shippers(ShipperID);
-
-ALTER TABLE
-    OrderDetails
-ADD
-    PRIMARY KEY (OrderID, ProductSK);
+CREATE
+OR REPLACE TABLE OrderDetails(
+    OrderID int NOT NULL,
+    ProductSK int NOT NULL FOREIGN KEY REFERENCES Products(ProductSK),
+    CustomerSK int NOT NULL FOREIGN KEY REFERENCES Customers(CustomerSK),
+    EmployeeSK int NOT NULL FOREIGN KEY REFERENCES Employees(EmployeeSK),
+    ShipperID int NOT NULL FOREIGN KEY REFERENCES Shippers(ShipperID),
+    OrderDateKey number(9) FOREIGN KEY REFERENCES DIM_DATE(DATE_PKEY),
+    RequiredDateKey number(9) FOREIGN KEY REFERENCES DIM_DATE(DATE_PKEY),
+    ShippedDateKey number(9) FOREIGN KEY REFERENCES DIM_DATE(DATE_PKEY),
+    ProductUnitPrice number NOT NULL,
+    ProductQuantity smallint NOT NULL,
+    ProductDiscount real NOT NULL,
+    Freight number,
+    ShipName varchar(40),
+    ShipAddress varchar(60),
+    ShipCity varchar(15),
+    ShipRegion varchar(15),
+    ShipPostalCode varchar(10),
+    ShipCountry varchar(15),
+    CONSTRAINT compositePK PRIMARY KEY (OrderID, ProductSK);
 
 --PRODUCTS
 CREATE
-OR REPLACE TABLE Products AS
-SELECT
-    TRY_TO_NUMBER(ProductID) AS ProductSK,
-    TRY_TO_NUMBER(ProductID) AS ProductID,
-    ProductName,
-    TQuantityPerUnit),
-    TRY_TO_NUMBER(UnitPrice) AS UnitPrice,
-    RY_TO_NUMBER(ReorderLevel) AS ReorderLevel,
-    Discontinued,
-    CategoryName,
-    Description AS CategoryDescription,
-    1 AS CurrentFlag,
-    CURRENT_TIMESTAMP(2) AS RecordStartDate,
-    CURRENT_TIMESTAMP(2) AS RecordEndDate
-FROM
-    Products
-    JOIN Categories ON Products.CategoryID = Categories.CategoryID;
-
-ALTER TABLE
-    Products
-ADD
-    PRIMARY KEY (ProductSK);
-
-CREATE TABLE DateDimension
+OR REPLACE TABLE Products(
+    ProductSK int DEFAULT AUTOINCREMENT(1, 1) PRIMARY KEY,
+    ProductID int NOT NULL,
+    ProductName varchar(40) NOT NULL,
+    QuantityPerUnit varchar(20),
+    UnitPrice number,
+    ReorderLevel smallint,
+    Discontinued boolean NOT NULL,
+    CategoryName varchar(15) NOT NULL,
+    CategoryDescription text,
+    CurrentFlag boolean DEFAULT 1,
+    RecordStartDate date DEFAULT CURRENT_DATE(),
+    RecordStartDate date DEFAULT CURRENT_DATE()
+);
