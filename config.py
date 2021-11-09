766008db9586755a3b6599c733c7004c863e26ea
Taras Smaliukh <62738836+Taras732@users.noreply.github.com>
2021-11-09 13:34:39 +0200

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-
-import mysql.connector
-
-def getConnection():
-    cnx = mysql.connector.connect(user='taras',
-                              password='taras_smaliukh',
-                              host='dbnorthwind.cjgt1chgkycw.eu-central-1.rds.amazonaws.com',
-                              database='northwind')
-    return cnx
