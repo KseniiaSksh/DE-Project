e2ed78b3ec185041ab52494e6cddc1622f230e5a
Taras Smaliukh <62738836+Taras732@users.noreply.github.com>
2021-11-10 16:30:35 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,25 @@
 import mysql.connector
 
-import boto
-import boto.s3.connection
+import boto3
+from boto3 import Session
 
-# Connection to db on rds
+# Connection to db in rds
 def getConnectionRDS():
-    cnx = mysql.connector.connect(user='<insert your DB user name>',
-                              password='insert your password to DB>',
-                              host='dbnorthwind.cjgt1chgkycw.eu-central-1.rds.amazonaws.com',
-                              database='northwind')
+    
+    cnx = mysql.connector.connect(user='xxxxxxxxxx',
+                              password='xxxxxxxxxxxxxxx',
+                              host='xxxxxxxxxxxxxxxxxx',
+                              database='xxxxxxxx')
     return cnx
 
 # Credentials to connect S3
-bucket_name = 'delabnortwind'
-access_key = '<insert your access key>'
-secret_key = '<insert your secret key>'
+bucket_name = 'xxxxxxxxxxx'
+access_key = 'xxxxxxxxxxxxx'
+secret_key = 'xxxxxxxxxxxxxxxx'
+
+client_S3 = boto3.client('s3',
+                         aws_access_key_id=access_key,
+                         aws_secret_access_key=secret_key)
+
+session = Session(aws_access_key_id=access_key,
+                  aws_secret_access_key=secret_key)
