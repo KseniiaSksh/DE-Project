1c612e5a707150d33bd121976ad27bb3f9b4aad1
Taras <taras.smalych@gmail.com>
2021-11-10 16:51:29 +0200

    New changes, solve problems with copying files

--- config.py
+++ config.py
@@ -1,9 +1,26 @@
-
 import mysql.connector
 
-def getConnection():
-    cnx = mysql.connector.connect(user='taras',
-                              password='taras_smaliukh',
-                              host='dbnorthwind.cjgt1chgkycw.eu-central-1.rds.amazonaws.com',
-                              database='northwind')
+import boto3
+from boto3 import Session
+
+# Connection to db in rds
+def getConnectionRDS():
+
+    cnx = mysql.connector.connect(user='xxxxxxxxxx',
+                              password='xxxxxxxxxxxxxxx',
+                              host='xxxxxxxxxxxxxxxxxx',
+                              database='xxxxxxxx')
     return cnx
+
+# Credentials to connect S3
+bucket_name = 'xxxxxxxxxxx'
+access_key = 'xxxxxxxxxxxxx'
+secret_key = 'xxxxxxxxxxxxxxxx'
+
+client_S3 = boto3.client('s3',
+                         aws_access_key_id=access_key,
+                         aws_secret_access_key=secret_key
+)
+
+session = Session(aws_access_key_id=access_key,
+                  aws_secret_access_key=secret_key)
