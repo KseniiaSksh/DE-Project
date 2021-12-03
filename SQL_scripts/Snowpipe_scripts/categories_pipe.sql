14d6d55898f9832b139a953fbd2e93889cfa0e00
Taras Smaliukh <62738836+Taras732@users.noreply.github.com>
2021-12-03 18:04:51 +0200

    Update categories_pipe.sql

--- SQL_scripts/Snowpipe_scripts/categories_pipe.sql
+++ SQL_scripts/Snowpipe_scripts/categories_pipe.sql
@@ -1,4 +1,4 @@
-create or replace pipe "NORTHWINDDB".STAGE_SCHEMA.categories_pipe 
+create or replace pipe "NORTHWINDDB".DWH_SCHEMA.categories_pipe 
 auto_ingest=true 
 as
 copy into "NORTHWINDDB".STAGE_SCHEMA.categories(
@@ -12,7 +12,10 @@ pattern='.*categories.*.csv'
 file_format = 'DE_LAB_CSV';
 
 //stage_table
+drop table categories;
 create or replace table categories(
 CategoriesID int,
 CategoryName varchar(40) NOT NULL,
-CategoryDescription varchar(100) NOT NULL)
+CategoryDescription varchar(100) NOT NULL);
+
+SELECT SYSTEM$PIPE_STATUS('categories_pipe'); 
